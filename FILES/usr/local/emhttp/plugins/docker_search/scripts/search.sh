e671abacd390380c2c7044332ef31a57f5588e36
Squidly271 <squidaz@sympatico.ca>
2015-03-22 22:09:54 -0400

    Final Updates for foreseeable future

--- FILES/usr/local/emhttp/plugins/docker_search/scripts/search.sh
+++ FILES/usr/local/emhttp/plugins/docker_search/scripts/search.sh
@@ -9,47 +9,72 @@ cat /tmp/search | while read LINE
 do
         if [ "$LINENUM" -eq "0" ]
         then
-# don't output the header line
+# Output the header line
                 LINENUM="1"
+		HEADERLINE=$(echo "$LINE" | sed 's/.\{9\}$//')
+		echo "<center><pre><code><font size=4><strong>                                  $HEADERLINE</strong></font></pre></code>"
+		echo "<hr />"
                 continue
         else
                 REPOSITORY=$(echo "$LINE" | cut -d " " -f1)
-       	        EVERYTHINGELSE=$(echo "$LINE" | sed 's/[^ ]* //' | sed 's/.\{25\}$//')
-               	echo "<font size=3><u><a href=\"https://registry.hub.docker.com/u/$REPOSITORY\" target=\"_blank\">https://registry.hub.docker.com/u/$REPOSITORY</a></u></font><span style='color: #E80000;'> $EVERYTHINGELSE</span> "
+       	        EVERYTHINGELSE=$(echo "$LINE" | sed 's/[^ ]* //' | sed 's/.\{4\}$//')
+               	echo "<center><pre><code><font size=4><u><a href=\"https://registry.hub.docker.com/u/$REPOSITORY\" target=\"_blank\">https://registry.hub.docker.com/u/$REPOSITORY</a></u><span style='color: #E80000;'> $EVERYTHINGELSE</span></pre></code></pre></center>"
 
 		if [[ $LINE != *"[OK]"* ]]
 		then
 			echo "<span style='color: #E80000;'>Incompatible with the conversion engine</span>"
 		fi
 
-		echo ""
+#		echo ""
         fi
 
 done
+
+echo "</font>"
 }
 
+# kill any error messages from docker search
 exec 2>/dev/null
-docker search --no-trunc=true --automated=true $1 > /tmp/search
+
+SEARCHTERM=$1
+
+# Convert search term to lower case.  Upper case is not allowed
+
+SEARCHTERM="$(echo "$SEARCHTERM" | tr '[:upper:]' '[:lower:]')"
+
+docker search --no-trunc=false --automated=true "$SEARCHTERM" > /tmp/search
 
 RESULTS=$(grep -c ^ /tmp/search)
 RESULTS=$((RESULTS-1))
 
+echo "<center><img src=/plugins/docker_search/images/dockerlogo.png></center>"
+
 if [ $RESULTS = -1 ]
 then
-	echo "<b><p><font size=6>Invalid Search Term</font></p></b>"
+	echo "<center><b><p><font size=6>Invalid Search Term $1</font></p></b><center>"
 	exit
 fi
 
-echo "<img src=/plugins/docker_search/images/dockerlogo.png>"
-echo "<b><p><font size=6>Search results for $1: $RESULTS</font></p>"
-echo "Search results may be limited by docker search.  More results may be available at on Docker's website<br>"
+if [ $RESULTS = 0 ]
+then
+	echo "<center><b><p><font size=6>There were no results for $1</font></p></b></center>"
+        echo "<center>Try broadening your search criteria</center>"
+
+        exit
+fi
+
+echo "<center><b><p><font size=6> Search results for $1: $RESULTS </font></p></b></center>"
+
+echo "<center>Search results may be limited by docker search.  More results may be available on Docker's website<br></center>"
 echo "<hr />"
 
 OutputResults
 
 echo "<hr />"
-echo "Clicking the link will take you to the associated docker page."
+echo "<center>Clicking the link will take you to the associated docker page.</center>"
 echo ""
-echo "Copy the link address, close the window and paste it into the \"Convert\" text box to convert the Docker File to an XML template for use in unRAID"
+echo "<center>Copy the link address, close the window and paste it into the \"Convert\" text box to convert the Docker File to an XML template for use in unRAID</center>"
 echo ""
+
+echo "<div style=\"text-align: right; direction: ltr; margin-left: 1em;\"><img src=/plugins/docker_search/images/Chode_300.gif></div>"
 
